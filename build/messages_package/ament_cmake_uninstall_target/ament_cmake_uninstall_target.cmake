267cad5c55aa21bc3f368406b2fe58ea15f519c2
kunalr97 <kunal.runwal@smail.inf.h-brs.de>
2021-12-10 14:14:28 +0100

    'Update Repo'

--- build/messages_package/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
+++ build/messages_package/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
@@ -2,6 +2,7 @@
 # ament_cmake_core/cmake/uninstall_target/ament_cmake_uninstall_target.cmake.in
 
 function(ament_cmake_uninstall_target_remove_empty_directories path)
+  set(install_space "/home/kunal/AST_Robot_Warehouse/install/messages_package")
   set(install_space "/home/sam/AST_Robot_Warehouse/install/messages_package")
   if(install_space STREQUAL "")
     message(FATAL_ERROR "The CMAKE_INSTALL_PREFIX variable must not be empty")
@@ -29,6 +30,7 @@ function(ament_cmake_uninstall_target_remove_empty_directories path)
 endfunction()
 
 # uninstall files installed using the standard install() function
+set(install_manifest "/home/kunal/AST_Robot_Warehouse/build/messages_package/install_manifest.txt")
 set(install_manifest "/home/sam/AST_Robot_Warehouse/build/messages_package/install_manifest.txt")
 if(NOT EXISTS "${install_manifest}")
   message(FATAL_ERROR "Cannot find install manifest: ${install_manifest}")
